ff3c9be050fd9d8b05cb32519b64664948fefef5
Artem <artem.kulchitsky@gmail.com>
2023-08-11 08:38:26 +0200

    Build: 0.5.4

---
+++ dist/assets/DocsView-1a96c6bd.js
@@ -0,0 +1,1 @@
+import{S as e}from"./StubSection-d829c046.js";import{o,c as t,e as a}from"./index-7ff5c2b5.js";const c={class:"main__container"},m={__name:"DocsView",setup(r){return(s,n)=>(o(),t("div",c,[a(e,{margin:""})]))}};export{m as default};
