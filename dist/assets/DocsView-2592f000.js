6bae27fbe8b0a20a676dc893387133e306644e28
Artem <artem.kulchitsky@gmail.com>
2023-08-17 07:30:48 +0200

    Build 0.5.8

--- dist/assets/DocsView-2592f000.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./StubSection-c38d3c04.js";import{o,c as t,e as a}from"./index-be480973.js";const c={class:"main__container"},m={__name:"DocsView",setup(r){return(s,n)=>(o(),t("div",c,[a(e,{margin:""})]))}};export{m as default};
--- dist/assets/UploadView-ed0fb2f6.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./StubSection-c38d3c04.js";import{o as a,c as t,e as o,b as s}from"./index-be480973.js";const c={class:"main__example example"},_=s("h1",{class:"example__title"},"Upload Files Example",-1),i={__name:"UploadView",setup(l){return(p,r)=>(a(),t("section",c,[_,o(e)]))}};export{i as default};
