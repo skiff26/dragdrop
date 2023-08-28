90fe5fd52f25e14647f394276da68d6120ec707c
Artem <artem.kulchitsky@gmail.com>
2023-08-28 08:25:11 +0200

    Add Quick Start page

--- src/views/docs/StartView.vue
+++ src/views/docs/StartView.vue
@@ -1,10 +1,19 @@
 <template>
 	<section class="main__article article">
 		<h1 class="article__title">Quick Start</h1>
-		<StubSection />
+		<p>Welcome to the Vue.js Ready-to-Use Drag and Drop Components! Our library offers a collection of drag and drop UI elements for Vue.js projects. No additional dependencies are required – simply select the component you need, copy its code, and paste it into your project.</p>
+		<h2>Getting Started</h2>
+		<p>Getting started with Vue.js Ready-to-Use Drag and Drop Components is a breeze:</p>
+		<br>
+		<ol>
+			<li class="small"><strong>Choose a Component:</strong> Browse through <router-link :to="{ name: 'simple' }">our collection</router-link> and select the component that matches your needs.</li>
+			<li class="small"><strong>Copy the Code:</strong> Each component comes with a code snippet. Simply copy the code provided in the documentation. <img src="/docs/copy-code.png" alt="Copy code button" loading="lazy" width="151"></li>
+			<li class="small"><strong>Paste into Your Project:</strong> Paste the code snippet into your Vue.js project's source code.</li>
+			<li class="small"><strong>Customize:</strong> Tailor the component's appearance and behavior to fit your project's design and functionality</li>
+		</ol>
+		<h2>Start Dragging and Dropping!</h2>
+		<p>With Vue.js Ready-to-Use Drag and Drop Components, enhancing your project's user interface has never been easier. Enjoy the seamless integration and flexibility our components offer. For more details and component options, check out our documentation.</p>
+		<br>
+		<p>Happy coding!</p>
 	</section>
 </template>
-
-<script setup>
-import StubSection from '@/components/StubSection.vue';
-</script>
