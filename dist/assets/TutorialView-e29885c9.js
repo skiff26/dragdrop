35903409880c405b9d3759f88f391b90a25a606f
Artem <artem.kulchitsky@gmail.com>
2023-08-31 10:52:13 +0200

    Build 0.7.3

--- dist/assets/TutorialView-e29885c9.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./StubSection-5e352135.js";import{o as t,c as a,e as o,b as c}from"./index-cdf3d270.js";const s={class:"main__article article"},r=c("h1",{class:"article__title"},"Tutorial",-1),p={__name:"TutorialView",setup(_){return(i,l)=>(t(),a("section",s,[r,o(e)]))}};export{p as default};
